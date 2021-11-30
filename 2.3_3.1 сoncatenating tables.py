95c074b32abb3f62fb1ced80605eac35a76e805b
Serg Owtschinnikow <owtschinnikow@rambler.ru>
2021-11-30 16:07:58 +0300

    add new homework 2.3_3.1 сoncatenating tables.py DONE

--- 2.3_3.1 сoncatenating tables.py
+++ 2.3_3.1 сoncatenating tables.py
@@ -18,13 +18,20 @@ Sample Output:
 import sys
 
 
+max_data = 0
+
+
+
 def сoncatenating_tables(data_number:int, call_number:int, data_original:list, call_commands_list:list):
-    parent, rank =make_set(data_number)
+    global max_data
+    parent, rank = make_set(data_number)
+    # print('parent_0, rank_0 = ', parent, rank)
     for bases in call_commands_list:
-        union_tree(bases[0+1], bases[1+1], parent, rank)
-
-    pass
-
+        # print('bases', bases)
+        union_tree(bases[1], bases[0], parent, rank, data_original)
+        # print('parent, rank = ', parent, rank)
+        # print('data_original', data_original, 'max_data_original', max(data_original))
+        print(max_data)
 
 def make_set(data_number):
     """
@@ -55,7 +62,7 @@ def find_i(i:int, parent:list) -> int:
     return i
 
 
-def union_tree(i:int, j:int, parent:list, rank:list):
+def union_tree(i:int, j:int, parent:list, rank:list, data_original):
     """
     Функция пробует объединять деревья, которые относятся к i и j.
     Если это уже одно дерево, то ничего не происходит
@@ -66,12 +73,21 @@ def union_tree(i:int, j:int, parent:list, rank:list):
     :param rank:
     :return:
     """
+    global max_data
     i_id, j_id = find_i(i, parent), find_i(j, parent)
     if i_id == j_id:
         return
     if rank[i_id-1] > rank[j_id-1]:
+        data_original[i_id-1] = data_original[i_id-1] + data_original[j_id-1]
+        if max_data < data_original[i_id-1]:
+            max_data = data_original[i_id-1]
+        data_original[j_id - 1] = 0
         parent[j_id-1] = i_id
     else:
+        data_original[j_id - 1] = data_original[i_id - 1] + data_original[j_id - 1]
+        if max_data < data_original[j_id - 1]:
+            max_data = data_original[j_id - 1]
+        data_original[i_id - 1] = 0
         parent[i_id-1] = j_id
         if rank[i_id-1] == rank[j_id-1]:
             rank[j_id-1] += 1
@@ -82,13 +98,14 @@ def main():
     Функция обрабатывает ввод в программу, запускает нужные функции
     :return:
     """
+    global max_data
     reader = (line.split() for line in sys.stdin)
     data_number, call_number = [int(i) for i in (next(reader))]  # Количество таблиц, количество вызовов таблиц
     data_original = [int(i) for i in (next(reader))]  # Начальное количество строк в таблицах
     call_commands_list = [list(map(int, next(reader))) for i in range(call_number)]  # команды вызовов таблиц для слияния (в / из)
-    # print(data_number, call_number, data_original)
-    # print((call_commands_list))
+    max_data = max(data_original)
     сoncatenating_tables(data_number, call_number, data_original,call_commands_list)
+
 
 
 if __name__ == '__main__':
