08aad180fdde36566573b137830ff62cad438758
Serg Owtschinnikow <owtschinnikow@rambler.ru>
2021-12-09 17:18:52 +0300

    add new homework 3.2_3.1 finding a pattern in text.py ver 2

--- 3.2_3.1 finding a pattern in text.py
+++ 3.2_3.1 finding a pattern in text.py
@@ -53,6 +53,40 @@ def made_hash(text):
     return (hash_key % number_p)
 
 
+def made_hash_simple(text):
+    """
+    Function made hash key wich size_spreadsheet for the text.
+    :param text:
+    :param size_spreadsheet:
+    :return:
+    """
+    hash_key = 0
+    for number, ascii in enumerate(to_ascii(text)):
+        hash_key += ascii  % number_p
+    return (hash_key % number_p)
+
+def made_hash_simple_2(text):
+    """
+    Function made hash key wich size_spreadsheet for the text.
+    :param text:
+    :param size_spreadsheet:
+    :return:
+    """
+    hash_key = 0
+    for number, ascii in enumerate(to_ascii(text)):
+        hash_key += ascii
+    return hash_key
+
+
+def made_hash_refresh(hash_key_old, old_char, new_char):
+    hash_key = hash_key_old - ord(old_char) + ord(new_char)
+    return ((hash_key + number_p) % number_p)
+
+
+def hash_refresh_ver_2(hash_key_old, old_char, new_char):
+    hash_key = hash_key_old - ord(old_char) + ord(new_char)
+    return ((hash_key + number_p) % number_p)
+
 
 def main():
     """
@@ -62,13 +96,21 @@ def main():
     reader = (line.split() for line in sys.stdin)
     pattern = next(reader)[0]  # Pattern
     text = next(reader)[0]  # Text
-    hash_pattern = made_hash(pattern)
 
-    for index, character in enumerate(text):
-        if index < len(text) - len(pattern)+1:
-            hash_pattern_in_text = made_hash(text[index : index + len(pattern)])
+    hash_pattern = made_hash_simple_2(pattern)
+    hash_pattern_in_text = made_hash_simple_2(text[0: len(pattern)])
+
+
+    for index, _character in enumerate(text):
+        if index < len(text) - len(pattern) + 1:
+            # print(index)
+            # print(hash_pattern_in_text)
             if hash_pattern == hash_pattern_in_text:
-                print(index, end=' ')
+                if pattern == text[index : index + len(pattern)]:
+                    print(index, end=' ')
+            hash_pattern_in_text = made_hash_refresh(hash_pattern_in_text,
+                                                   _character,
+                                                    text[index + len(pattern) - 1])
 
 
 if __name__ == '__main__':
